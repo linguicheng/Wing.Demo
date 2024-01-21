c79f0a5bd52decf7441aca403cf48f9dfb3c43d7
linguicheng <2377087481@qq.com>
2024-01-21 17:05:27 +0800

    网关支持JWT策略授权

---
+++ 3.7/Program.cs
@@ -0,0 +1,13 @@
+using Wing;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.AddWing(builder => builder.AddConsul());
+
+builder.Services.AddControllers();
+
+builder.Services.AddWing().AddGateWay();
+
+var app = builder.Build();
+
+app.Run();
